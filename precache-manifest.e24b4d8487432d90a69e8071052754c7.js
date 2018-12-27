c83553f92bdb8f62ace0d1e447792e6870102d2c
Stephanie Yang <stephanie.yuxin.yang@gmail.com>
2018-12-27 18:19:33 -0500

    Updates

---
+++ precache-manifest.e24b4d8487432d90a69e8071052754c7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "dd9a3088eeb30c69cf1bfbeaece20223",
+    "url": "/cookie-clicker/static/media/cookie.dd9a3088.png"
+  },
+  {
+    "revision": "f59c9c8d98b4dc6753ad",
+    "url": "/cookie-clicker/static/js/runtime~main.f59c9c8d.js"
+  },
+  {
+    "revision": "1f5d771b0ac29c88e2f0",
+    "url": "/cookie-clicker/static/js/main.1f5d771b.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/cookie-clicker/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "1f5d771b0ac29c88e2f0",
+    "url": "/cookie-clicker/static/css/main.8ea60052.chunk.css"
+  },
+  {
+    "revision": "586bece5ec90c5885442275dd1a4401d",
+    "url": "/cookie-clicker/index.html"
+  }
+];
